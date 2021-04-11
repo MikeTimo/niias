c7d339e618720b68a4cff4ef623389272776390f
Timofeev Mikhail <mike18tim@gmail.com>
2021-04-11 19:28:07 +0300

    Добавил класс Main и Contriller

---
+++ src/main/kotlin/com/example/restservice/controller/Controller.kt
@@ -0,0 +1,19 @@
+package com.example.restservice.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @GetMapping
+    fun get() {
+
+    }
+
+    @PostMapping
+    fun post() {
+
+    }
+}
