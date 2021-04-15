d09fbed7d50f766ce46111e29d557f657dfdc569
Timofeev Mikhail <mike18tim@gmail.com>
2021-04-15 23:18:53 +0300

    Добавил зависимость OpenApi

--- src/main/kotlin/com/example/restservice/controller/Controller.kt
+++ src/main/kotlin/com/example/restservice/controller/Controller.kt
@@ -8,17 +8,17 @@ import java.time.Month
 
 import java.time.LocalDateTime
 
-@RestController("/schedule")
+@RestController
 class Controller {
 
-    @GetMapping
+    @GetMapping("/schedule")
     fun get(@RequestParam(value = "data") data: LocalDate): Schedule {
         val dataFrom = LocalDateTime.of(2017, Month.JULY, 9, 11, 6, 0)
         val dataTo = LocalDateTime.of(2017, Month.JULY, 9, 13, 15, 0)
         return Schedule(2, 37, 5, dataFrom, 10, dataTo)
     }
 
-    @PostMapping
+    @PostMapping("/schedule")
     @ResponseStatus(HttpStatus.OK)
     fun post(@RequestBody schedule: Schedule): String {
         return "OK"
