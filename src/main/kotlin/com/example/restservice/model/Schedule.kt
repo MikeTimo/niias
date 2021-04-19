2b304a4a4c4de79aa8804ab7c266c57ccc582bbd
Timofeev Mikhail <mike18tim@gmail.com>
2021-04-19 20:10:33 +0300

    Вынес все поля класса Schedule в конструктор

--- src/main/kotlin/com/example/restservice/model/Schedule.kt
+++ src/main/kotlin/com/example/restservice/model/Schedule.kt
@@ -2,25 +2,11 @@ package com.example.restservice.model
 
 import java.time.LocalDateTime
 
-class Schedule {
-    private var trainNumber: Int = 0
-    private var driverNumber: Int = 0
-    private var stationFrom: Int = 0
-    private var timeFrom: LocalDateTime? = null
-    private var stationTo: Int = 0
-    private var timeTo: LocalDateTime? = null
-
-    constructor()
-
-    constructor(
-        trainNumber: Int, driverNumber: Int, stationFrom: Int, timeFrom: LocalDateTime,
-        stationTo: Int, timeTo: LocalDateTime
-    ) : this() {
-        this.trainNumber = trainNumber
-        this.driverNumber = driverNumber
-        this.stationFrom = stationFrom
-        this.timeFrom = timeFrom
-        this.stationTo = stationTo
-        this.timeTo = timeTo
-    }
-}
+class Schedule(
+    private var trainNumber: Int,
+    private var driverNumber: Int,
+    private var stationFrom: Int,
+    private var timeFrom: LocalDateTime,
+    private var stationTo: Int,
+    private var timeTo: LocalDateTime?
+)
